ca600a3b32234164d013aea412ab4c1d2d10649e
Nathan <nwong@risd.edu>
2022-05-05 11:44:38 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Native LazyLoad with Fade in
+ Intersection observer for native browser lazy loaded images with a nice CSS fade in.
---
+++ lazy-load.js
@@ -0,0 +1,18 @@
+/** First we get all the non-loaded image elements **/
+var lazyImages = [].slice.call(document.querySelectorAll('img[loading="lazy"]'));
+/** Then we set up a intersection observer watching over those images and whenever any of those becomes visible on the view then replace the placeholder image with actual one, remove the non-loaded class and then unobserve for that element **/
+let lazyImageObserver = new IntersectionObserver(function(entries, observer) {
+  entries.forEach(function(entry) {
+    if (entry.isIntersecting) {
+      let lazyImage = entry.target;
+      lazyImage.classList.add("loaded");
+      lazyImageObserver.unobserve(lazyImage);
+    }
+  });
+}, {
+  threshold: 0.15
+});
+/** Now observe all the non-loaded images using the observer we have setup above **/
+lazyImages.forEach(function(lazyImage) {
+  lazyImageObserver.observe(lazyImage);
+});
